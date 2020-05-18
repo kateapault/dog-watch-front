723ceac60b71f832311eb052635de6302c37fed3
github username <kateapault@gmail.com>
2020-05-18 12:20:16 -0400

    installed react-navigation

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import HomeScreen from './screens/HomeScreen'
+import AddEventNavigator from './navigation/AddEventNavigator'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <AddEventNavigator />
     </View>
     
   );
